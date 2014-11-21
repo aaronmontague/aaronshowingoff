269517993d3d0d05580d01e163e669d2b50a23b3
Aaron Montague <montaa@alum.rpi.edu>
2014-11-21 14:33:30 -0500

    adding a non WP folder

---
+++ aaronshowingoff/aaronshowingoff/exercises/Index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Index short summary.
+ *
+ * Index description.
+ *
+ * @version 1.0
+ * @author Aaron
+ */
+class Index
+{
+    
+}
+
+?>
+
+<html>
+    <body>
+        Hello There!
+    </body>
+</html>
