18eb82e9f8e2da78d10b8a19a835cdce553b9431
Aaron Montague <montaa@alum.rpi.edu>
2014-11-21 17:25:48 -0500

    basic xml reader installed

--- aaronshowingoff/aaronshowingoff/exercises/reading_xml.php
+++ aaronshowingoff/aaronshowingoff/exercises/reading_xml.php
@@ -5,13 +5,17 @@
 </head>
 <body>
     Gas Pricing by State:
+    <br />
     <?php
     $xml=simplexml_load_file("natural_gas.xml") or die("Error: Cannot create object");
-    echo $xml->id . "<br>";
-    echo $xml->title . "<br>";
-    echo $xml->d . "<br>";
-    echo $xml->cng . "<br>";
-    echo $xml->lng;
+        //echo $xml->states->state[1]->title; 
+    foreach($xml->states->state as $state) {
+        //echo "State: " . $state->id . "<br>";
+        echo "State: " . $state->title . "<br>";
+        echo "Diesel Price: " . $state->d . "<br>";
+        echo "CNG Price: " . $state->cng . "<br>";
+        echo "LNG Price: " . $state->lng . "<br><br>";
+    }
     ?>
 </body>
 </html>
