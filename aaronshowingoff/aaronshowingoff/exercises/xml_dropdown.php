d9a6f195b91bbb65e8acf7bad576b85d5b00e13c
Aaron Montague <montaa@alum.rpi.edu>
2014-11-23 04:05:47 -0500

    added jscript xml parser - not working

--- aaronshowingoff/aaronshowingoff/exercises/xml_dropdown.php
+++ aaronshowingoff/aaronshowingoff/exercises/xml_dropdown.php
@@ -2,10 +2,38 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Parsing XML into a Dropdown</title>
+    <script type="text/javascript">
+        function update_prices(dog, url) {
+            //alert(dog.selectedIndex)
+            var xmlhttp;
+            var cows, bird, apple;
+            xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    cows = "";
+                    bird = xmlhttp.responseXML.documentElement.getElementsByTagName("state");
+                    apple = bird[dog.selectedIndex].getElementsByTagName("id");
+                    try {
+                        cows += "State: " + apple[0].firstChild.nodeValue + "<br>";
+                    }
+                    catch (er) {
+                        cows += "State didn't work..." + dog.selectedIndex + "<br>"
+                    }
+                    cows += "Diesel Price: " + " xxx<br>";
+                    cows += "CNG Price: " + " xxx<br>";
+                    cows += "LNG Price: " + " xxx<br>";
+                    document.getElementById('display_prices').innerHTML = cows;
+                }
+            }
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+
+        }
+    </script>
 </head>
 <body>
     Select a State:
-    <select>
+    <select id="state_select" onchange="update_prices(this,'natural_gas.xml')">
         <?php
         $xml=simplexml_load_file("natural_gas.xml") or die("Error: Cannot create object");
         //echo $xml->states->state[1]->title; 
@@ -15,7 +43,17 @@
         }
         ?>
     </select>
+    <br />
+    <div id="display_prices">
+        State: 
+        <br />
+        LNG Price:
+        <br />
+        CNG Price:
+        <br />
+        Diesel Price:
+        <br />
+    </div>
 
-    <!-- create a div to display the prices in various states | perform minor calculations as well -->
 </body>
 </html>
