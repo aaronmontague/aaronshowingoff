83bc7040c8ffd10c00f85e797fd038578e8d06d9
Aaron Montague <montaa@alum.rpi.edu>
2014-12-03 01:02:42 -0500

    small index change

--- aaronshowingoff/aaronshowingoff/exercises/Index.php
+++ aaronshowingoff/aaronshowingoff/exercises/Index.php
@@ -1,9 +1,9 @@
 <html>
     <body>
-        <h2>Various PHP Exercises:</h2>
+        <h2>PHP XML Exercises:</h2>
         <a href="reading_xml.php">XML Parsing</a><br />
         <a href="xml_dropdown.php">XML Parsing into a Dropdown</a><br />
-        <h2>Cookie Exercises:</h2>
+        <h2>PHP Cookie Exercises:</h2>
         <a href="cookie_set.php">Cookie Setting</a><br />
         <a href="cookie_change.php">Cookie Value Change</a><br />
         <a href="cookie_check.php">Cookie Value Check</a><br />
